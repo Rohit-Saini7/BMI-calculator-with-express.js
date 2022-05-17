ec1b934c696a21d7604f6e3dc084181c019a2eda
Rohit <rohit712wd@gmail.com>
2022-05-17 20:34:01 +0530

    calculator page add

--- index.js
+++ index.js
@@ -9,6 +9,18 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + "/public/index.html");
 });
 
+app.get("/bmicalc", function (req, res) {
+  res.sendFile(__dirname + "/public/bmicalc.html");
+});
+
+app.post("/bmicalc", function (req, res) {
+  var w = parseFloat(req.body.weight);
+  var h = parseFloat(req.body.height);
+  var bmi = w / (h * h);
+
+  res.send("BMI is " + bmi);
+});
+
 app.listen(3000, function () {
   console.log("Server is successfully started.");
 });
